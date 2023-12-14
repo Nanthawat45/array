acf2680805824e268dd8e983244447241e815a2c
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2023-12-14 14:18:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+  const cars = ["Saab", "Volvo", "BMW", "TOYOTA"]; 
+  console.log(cars[0]); 
+  documet.write(cars[2]);
+  window.alert(cars[1]);
+  cars.push("TOYOTA1")
+  cars.pop(cars[1])
+</script>;
